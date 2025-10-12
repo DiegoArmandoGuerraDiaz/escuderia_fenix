9e4aa90d48fbeb71be414e44ae321e384f8bea1e
Diego Armando Guerra Diaz <diego.armando.guerra.diaz@gmail.com>
2025-10-11 21:32:24 -0600

    commit inicial

--- src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-escuderia-gris text-gray-400 py-6 text-center">
-      <p>© {new Date().getFullYear()} Escudería Fénix. Todos los derechos reservados.</p>
-    </footer>
-  )
-}
